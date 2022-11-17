93012de3335bc33fc4137dd51645cf7dad2814a7
Yann Decelle <yann.decelle@edu.devinci.fr>
2022-11-17 12:00:17 +0100

    added script for enigmas and style

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// when the button with class btn is clicked turn every element with class .box to red
+
+var btn = document.querySelector('.sprayPaint');
+var box = document.querySelectorAll('.code');
+
+btn.addEventListener('click', function() {
+    for (var i = 0; i < box.length; i++) {
+        box[i].style.color = 'red';
+    }
+    }
+);
+
+var code = "psacs"
+
+// if the answer in the input is correct, change the background color of the body to green
+
+var btn1 = document.querySelector('.answer');
+var input1 = document.querySelector('.input');
+var body1 = document.querySelector('body');
+
+btn.addEventListener('click', function() {
+    if (input.value === code) {
+        var enigma1 = document.querySelector('.enigma1');
+        var enigma2 = document.querySelector('.enigma2');
+        enigma1.style.display = 'none';
+        enigma2.style.display = 'block';   
+    }
+}
+);
